1ac0e5c3b305d52d3c07d2eea509b45318d53071
Yan Ren <ryan.ren3@gmail.com>
2022-05-10 10:44:36 -0400

    add executable

--- comp6411/src/main/java/com/comp6411/Main.java
+++ comp6411/src/main/java/com/comp6411/Main.java
@@ -2,7 +2,12 @@ package com.comp6411;
 
 public class Main {
     public static void main(String[] args) {
-        String path = Main.class.getClassLoader().getResource("rand.txt").getPath();
+        String path;
+        if (args.length == 0) {
+            path = Main.class.getClassLoader().getResource("rand.txt").getPath();
+        } else {
+            path = args[0];
+        }
         new HeapSort(path).start();
     }
 }
